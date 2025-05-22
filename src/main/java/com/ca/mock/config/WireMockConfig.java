82d5ae02ae9810026137feea0f82fea3a3f75050
vijay.dubey <vjobdubey@gmail.com>
2025-05-22 19:41:47 +0530

    updated class path config

--- src/main/java/com/ca/mock/config/WireMockConfig.java
+++ src/main/java/com/ca/mock/config/WireMockConfig.java
@@ -1,21 +1,28 @@
 package com.ca.mock.config;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
-import com.github.tomakehurst.wiremock.common.ClasspathFileSource;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.URL;
 
 @Configuration
 public class WireMockConfig {
 
     @Bean(initMethod = "start", destroyMethod = "stop")
     public WireMockServer wireMockServer() {
+      System.out.println(System.getProperty("java.class.path"));
+        ClassLoader classLoader = getClass().getClassLoader();
+        URL file = classLoader.getResource(".");
+        System.out.println("Path::::::"+ file.getPath());
+        if (file == null) {
+            throw new RuntimeException("wiremock resource not found in classpath");
+        }
                 return new WireMockServer(
                         WireMockConfiguration.
                                 options()
                                 .port(8082)
-                                .fileSource(new ClasspathFileSource(".")));
-
+                                .usingFilesUnderClasspath(file.getPath()));
     }
 }
