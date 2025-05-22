969a0fac21dd2f8be49008049494c2d0f8d90c2f
vijay.dubey <vjobdubey@gmail.com>
2025-05-22 23:44:08 +0530

    updated usingFilesUnderClasspath location

--- src/main/java/com/ca/mock/config/WireMockConfig.java
+++ src/main/java/com/ca/mock/config/WireMockConfig.java
@@ -13,17 +13,11 @@ public class WireMockConfig {
 
     @Bean(initMethod = "start", destroyMethod = "stop")
     public WireMockServer wireMockServer() {
-      System.out.println(System.getProperty("java.class.path"));
-        ClassLoader classLoader = getClass().getClassLoader();
-        URL file = classLoader.getResource(".");
-        System.out.println("Path::::::"+ file.getPath());
-        if (file == null) {
-            throw new RuntimeException("wiremock resource not found in classpath");
-        }
-                return new WireMockServer(
+                    return new WireMockServer(
                         WireMockConfiguration.
                                 options()
                                 .port(8082)
-                                .usingFilesUnderClasspath(file.getPath()));
+                                .usingFilesUnderClasspath("BOOT-INF/classes/mappings")
+                );
     }
 }
